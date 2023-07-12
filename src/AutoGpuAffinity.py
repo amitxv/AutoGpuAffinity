7ffaefdbaaaa4ff97dee76139f62bf8f16f9fadb
AMIT <86924251+amitxv@users.noreply.github.com>
2023-07-12 20:34:30 +0100

    Update CLI output message

--- src/AutoGpuAffinity.py
+++ src/AutoGpuAffinity.py
@@ -186,7 +186,7 @@ def display_results(csv_directory: str, enable_color: bool) -> None:
 def main() -> int:
     version = "0.15.5"
 
-    print(f"AutoGpuAffinity Version {version}\nCopyright (C) github.com/amitxv. All rights reserved.\n")
+    print(f"AutoGpuAffinity Version {version} - GPLv3\nGitHub - https://github.com/amitxv\n")
 
     if not ctypes.windll.shell32.IsUserAnAdmin():
         print("error: administrator privileges required")
