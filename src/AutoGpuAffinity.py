667b2df2a1474c20b59c6ec41b8772e8d7120076
AMIT <86924251+amitxv@users.noreply.github.com>
2023-12-18 16:25:36 +0000

    Update version

--- src/AutoGpuAffinity.py
+++ src/AutoGpuAffinity.py
@@ -193,7 +193,7 @@ def display_results(csv_directory: str, enable_color: bool) -> None:
 
 
 def main() -> int:
-    version = "0.15.9"
+    version = "0.15.10"
 
     print(f"AutoGpuAffinity Version {version} - GPLv3\nGitHub - https://github.com/amitxv\n")
 
