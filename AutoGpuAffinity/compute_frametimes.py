b286769830e188e67c39f912c05880746fe31832
AMIT <86924251+amitxv@users.noreply.github.com>
2024-03-04 10:44:46 +0000

    remove class function "self" annotation

--- AutoGpuAffinity/compute_frametimes.py
+++ AutoGpuAffinity/compute_frametimes.py
@@ -1,9 +1,8 @@
 import math
-from typing import Self
 
 
 class Fps:
-    def __init__(self: Self, frametimes: list[float]) -> None:
+    def __init__(self, frametimes: list[float]) -> None:
         self.sorted_frametimes = sorted(frametimes, reverse=True)
 
         # cache values
@@ -11,7 +10,7 @@ class Fps:
         self.length = len(frametimes)
         self.mean = 1000 / (self.total / self.length)
 
-    def lows(self: Self, value: float) -> float:
+    def lows(self, value: float) -> float:
         current_total = 0.0
 
         for frametime in self.sorted_frametimes:
@@ -20,18 +19,18 @@ class Fps:
                 return 1000 / frametime
         return 0.0
 
-    def percentile(self: Self, value: float) -> float:
+    def percentile(self, value: float) -> float:
         return 1000 / self.sorted_frametimes[math.ceil(value / 100 * self.length) - 1]
 
-    def stdev(self: Self) -> float:
+    def stdev(self) -> float:
         squared_deviations = sum((1000 / framerate - self.mean) ** 2 for framerate in self.sorted_frametimes)
         return math.sqrt(squared_deviations / (self.length - 1))  # bessel's correction
 
-    def maximum(self: Self) -> float:
+    def maximum(self) -> float:
         return 1000 / self.sorted_frametimes[-1]
 
-    def minimum(self: Self) -> float:
+    def minimum(self) -> float:
         return 1000 / self.sorted_frametimes[0]
 
-    def average(self: Self) -> float:
+    def average(self) -> float:
         return self.mean
