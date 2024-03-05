69559ca7be77b9ddcc51e1328ba78a75a0066afe
AMIT <86924251+amitxv@users.noreply.github.com>
2024-03-05 10:19:02 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -2,8 +2,12 @@ function main() {
     # pack executable
     pyinstaller AutoGpuAffinity\main.py --onefile --name AutoGpuAffinity
 
+    if (Test-Path "building") {
+        Remove-Item -Path "building" -Recurse
+    }
+
     # create folder structure
-    mkdir building\AutoGpuAffinity
+    New-Item -ItemType Directory -Path building\AutoGpuAffinity
     Move-Item dist\AutoGpuAffinity.exe building\AutoGpuAffinity
     Move-Item AutoGpuAffinity\bin building\AutoGpuAffinity
     Move-Item AutoGpuAffinity\config.ini building\AutoGpuAffinity
